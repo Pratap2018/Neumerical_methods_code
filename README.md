87c2fe600128055b15d1de3f9776783144ff6700
Pratap2018 <dcat9816@gmail.com>
2019-03-15 12:43:19 +0530

    Bisection Method range spcify

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Neumerical_methods_code
+
 This is Clg assignment related....
+=========================================
+bisection method :
+input: a and b (provide a range of a and b)
+output: root
--- bisection.c
+++ bisection.c
@@ -4,7 +4,7 @@
 #define E .000001
 
 double func(double x){
- return  ((pow(x,3)-x-1));
+ return  ((pow(x,3)-x-1));// provide the equation
 }
 double bisection(double a,double b){
     return (a+b)/2;
@@ -15,10 +15,11 @@ int i=0;
 double a,b,xr;
 printf("Enter the values of a and b : " );
 scanf("%lf %lf",&a,&b );
+/****************************************************
 if (func(a)*func(b)>0) {
   perror("Error:THE root must be in interval");
   exit(1);
-}
+}****************************************************/
 xr=bisection(a,b);
 while((b-a)>=E){
 i++;
@@ -32,9 +33,6 @@ if (func(xr)*func(a)<0) {
   a=xr;
 }
 }
-
 printf("After %d interval : THE root is %lf\n",i,xr  );
-
-
 
 }
